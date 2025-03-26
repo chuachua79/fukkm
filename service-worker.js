f6bdeecbca7b96ff2bae772baaae0b89eba857d3
pfbsarikei <harrycks@moh.gov.my>
2025-03-26 12:35:35 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-const CACHE_NAME = "pwa-cache-v1";
-const URLS_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/app.js",
-    "/service-worker.js"
-];
-
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(URLS_TO_CACHE))
+        caches.open('drug-info-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/app.js',
+                '/manifest.json'
+            ]);
+        })
     );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
 });
